06afaeecf42745c619d790e8c2232cb85a9c7c92
MitchellJC <81349046+MitchellJC@users.noreply.github.com>
2023-07-25 17:45:00 +1000

    Style updates for mobile

--- static/components/gameField.js
+++ static/components/gameField.js
@@ -13,17 +13,19 @@ class GameField extends HTMLElement {
         this.innerHTML = 
         `<div class="game-field">
             <button type="button" class="rem-pref">&#10005</button>
-            <div>
+
             <label for="game-title${this.id_}">Game title:</label>
             <input type="text" name="game-title" id="game-title${this.id_}" class="game-title" required>
-            </div>
-            <div>
-            <label for="dislike-option${this.id_}">Dislike</label>
-            <input class="dislike-radio" type="radio" name="like-box${this.id_}" id="dislike-option${this.id_}" value="dislike">
-            </div>
-            <div>
-            <label for="like-option${this.id_}">Like</label>
-            <input class="like-radio" type="radio" name="like-box${this.id_}" id="like-option${this.id_}" value="like" checked>
+            
+            <div class="pref-box">
+                <div class="label-radio-cont">
+                    <label for="dislike-option${this.id_}">Dislike</label>
+                    <input class="dislike-radio" type="radio" name="like-box${this.id_}" id="dislike-option${this.id_}" value="dislike">
+                </div>
+                <div class="label-radio-cont">
+                    <label for="like-option${this.id_}">Like</label>
+                    <input class="like-radio" type="radio" name="like-box${this.id_}" id="like-option${this.id_}" value="like" checked>
+                </div>
             </div>
            <div class="search-result"></div>
         </div>`;
