9b5d62751ee7f86c6e5faa0d95403da59b09d181
MitchellJC <81349046+MitchellJC@users.noreply.github.com>
2023-09-01 21:30:00 +1000

    Added some documentation to KNN classes

--- KNN.py
+++ KNN.py
@@ -5,7 +5,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 import random
 
 @jit(nopython=True)
-def compute_sims(item_ratings, num_users, num_items, means=None):
+def compute_sims(item_ratings, num_users, num_items, means=None) -> np.Array:
     sims = np.zeros((num_items, num_items))
 
     for i in range(num_items):
@@ -18,7 +18,8 @@ def compute_sims(item_ratings, num_users, num_items, means=None):
                     sims[i, j] = -2
                     continue
                 means_prime =(means[i], means[j]) if means != None else (0, 0)
-                sims[i, j] = cos_sim(item_ratings[i], item_ratings[j], num_users, means_prime)
+                sims[i, j] = cos_sim(item_ratings[i], item_ratings[j], num_users, 
+                                     means_prime)
     
     return sims
 
@@ -100,28 +101,61 @@ def ensemble_top_n(n , sims_pairs, user_prefs, num_items, k):
     return top
 
 class ItemKNN:
-    def __init__(self, k=5, mean_centered=False, iuf=False):
+    """Item-based neighbourhood model for item recommendation."""
+    def __init__(self, k: int = 40, mean_centered: bool = False, 
+                 iuf: bool = False) -> None:
+        """Initialise new ItemKNN instance.
+        
+        Parameters:
+            k - Number of neighbours
+            mean_centered - True to mean center ratings.
+            iuf - True to use inverse user frequency factor.
+        """
         self._k = k
         self._mean_centered = mean_centered
         self._item_means = None
         self._iuf = iuf
 
-    def fit(self, M):
+    def fit(self, M: np.Array) -> None:
+        """Train the model using utility matrix M.
+
+        Parameters:
+            M - UxI utility matrix containing user-item ratings. U is the number
+                of users, I is the number of items.
+        """
         self._M = M
         self._num_users, self._num_items = self._M.shape
         self._store_rating_pairs(self._M)
         if self._mean_centered:
             self._store_item_means(self._M)
         
-        self._sims = compute_sims(self._item_ratings, self._num_users, self._num_items, means=self._item_means)
+        self._sims = compute_sims(self._item_ratings, self._num_users, 
+                                  self._num_items, means=self._item_means)
 
-    def top_n(self, user, n, prefs=None):
-        prefs = self._M[[user], :] if prefs == None else prefs
+    def top_n(self, user: int, n: int, prefs: np.Array = None) -> (
+            list[tuple[float, int]]):
+        """Return the top-n recommendations for user.
+        
+        Parameters:
+            user - id of user.
+            n - Number of recommendations to generate.
+            prefs - 1xI array of user-item ratings for the given user. Default
+                will try to use ratings from training.
+
+        Returns:
+            List of recommendations in the form (prediction, item_id)
+        """
+        try:
+            prefs = self._M[[user], :] if prefs == None else prefs
+        except IndexError:
+            print("Could not find user. Please provide preference array.")
+            return
+        
         prefs = prefs.tocsr()
         top = top_n(n, self._sims, prefs, self._num_items, self._k, iufs=self._iufs)
         return top
 
-    def _store_rating_pairs(self, M):
+    def _store_rating_pairs(self, M: np.Array) -> None:
         print("Storing ratings in dictionary...")
         # Type defs
         index_type = nb.types.int64
@@ -151,9 +185,10 @@ class ItemKNN:
 
             self._user_ratings[user][item] = M[user, item]
             self._item_ratings[item][user] = M[user, item]
+
         print("Done storing in dictionary.")
 
-    def _store_item_means(self, M):
+    def _store_item_means(self, M: np.Array) -> None:
         print("Computing item means...")
         index_type = nb.types.int64
         rating_type = nb.types.float64
@@ -167,10 +202,9 @@ class ItemKNN:
 
             if i in self._item_ratings:
                 ratings = self._item_ratings[i].values()
-                # ratings = users.values()
                 mean = sum(ratings)/len(self._item_ratings[i])
-
-                self._iufs[i] = np.emath.logn(4, self._num_users/(len(self._item_ratings[i])))
+                self._iufs[i] = np.emath.logn(4, self._num_users
+                                              /(len(self._item_ratings[i])))
             else:
                 mean = 0
 
@@ -178,27 +212,83 @@ class ItemKNN:
         print("Done computing item means.")
             
 class ContentKNN:
-    def __init__(self, k=40):
+    """Content-based neighbourhood model for item recommendation."""
+    def __init__(self, k: int = 40) -> None:
+        """Initialise new ContentKNN instance.
+        
+        Parameters:
+            k - Number of neighbours
+        """
         self._k = k
 
-    def fit(self, X):
+    def fit(self, X: np.Array) -> None:
+        """Train the model using feature array X.
+
+        Parameters:
+            X - IxF feature array. I is the number of items. F is the number of
+                features.
+        """
         self._sims = cosine_similarity(X)
         self._num_items = self._sims.shape[0]
 
-    def top_n(self, user, n, prefs=None):
+    def top_n(self, user: int, n: int, prefs: np.Array = None) -> list[
+        tuple[float, int]]:
+        """Return the top-n recommendations for user.
+        
+        Parameters:
+            user - id of user.
+            n - Number of recommendations to generate.
+            prefs - 1xI array of user-item ratings for the given user. Must be
+                given.
+
+        Returns:
+            List of recommendations in the form (prediction, item_id)
+        """
+        if not prefs:
+            print("Please give prefs array.")
+            return
         prefs = prefs.tocsr()
         top = top_n(n, self._sims, prefs, self._num_items, self._k)
         return top
 
 class EnsembleKNN:
-    def __init__(self, k=40):
+    """Ensembler for different neighbourhood models. Takes weighted average of 
+    each model to make predictions.
+    """
+    def __init__(self, k: int = 40):
+        """Initialise new EnsembleKNN instance.
+        
+        Parameters:
+            k - Number of neighbours
+        """
         self._k = k
 
-    def set_sims(self, sim_pairs):
+    def set_sims(self, sim_pairs: list[tuple[np.Array, float]]) -> None:
+        """Set the similarity from each neighbourhood model.
+        
+        Parameters:
+            sim_pairs - List of tuples of the form (similarities, weight).
+                Where similarites is a IxI similarity matrix and weight is
+                is the desired weight of prediction from model associated with
+                similarities."""
         self._num_items = sim_pairs[0][0].shape[0]
         self._sim_pairs = sim_pairs
 
-    def top_n(self, user, n, prefs=None):
+    def top_n(self, user: int, n: int, prefs: np.Array = None):
+        """Return the top-n recommendations for user.
+        
+        Parameters:
+            user - id of user.
+            n - Number of recommendations to generate.
+            prefs - 1xI array of user-item ratings for the given user. Must be
+                given.
+
+        Returns:
+            List of recommendations in the form (prediction, item_id)
+        """
+        if not prefs:
+            print("Please give prefs array.")
+            return
         prefs = prefs.tocsr()
         top = ensemble_top_n(n, self._sim_pairs, prefs, self._num_items, self._k)
         return top
